807a1c2e2dd863248393e954b9a35e1808d2d5d9
parameswar-patra <parameswarapatra19@gmail.com>
2025-05-09 08:19:29 +0530

    Update test.sh

--- Day-10-userdata/test.sh
+++ Day-10-userdata/test.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start  
 sudo systemctl enable httpd
-echo "<h1>Welcome to Naresh IT ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+echo "<h1>Welcome to Naresh IT ! AWS Infra created using Terraform in ap-south-1 Region</h1>" > /var/www/html/index.html
